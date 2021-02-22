966e741ea5de0b7f299f5eaecd7c37729588f626
Joshua Maney <joshuaraymaney@gmail.com>
2021-02-21 20:52:47 -0500

    converted arrow functions to normal functions so i could understand them better

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,52 @@
 var questions = [
-        {
-            question: "Which of the following is correct about features of JavaScript?",
-            choice1: "JavaScript is complementary to and integrated with HTML",
-            choice2: "JavaScript is open and cross-platform",
-            choice3: "Both of the above",
-            choice4: "None of the above",
-            answer: "Both of the above"
-        },
-        {
-            question: "Which built-in method removes the last element from an array and returns that element?",
-            choice1: "last()",
-            choice2: "get()",
-            choice3: "pop()",
-            choice4: "None of the above",
-            answer: "pop()"
-        },
-        {
-            question: "Which of the following functions splits objects into an array of strings",
-            choice1: "slice()",
-            choice2: "split()",
-            choice3: "replace()",
-            choice4: "search()",
-            answer: "split()"
-        },
-        {
-            question: "Which functions returns the string value as upper case?",
-            choice1: "toLocaleUpperCase()",
-            choice2: "toUpperCase()",
-            choice3: "toString()",
-            choice4: "subString()",
-            answer: "toUpperCase()"
-        },
-        {
-            question: "____ tag is an extension of HTML that can enclose any number of JavaScript statements",
-            choice1: "<script>",
-            choice2: "<body>",
-            choice3: "<head>",
-            choice4: "<title>",
-            answer: "<script>"
-        },
-    ]
+    {
+        question: "Which of the following is correct about features of JavaScript?",
+        choice1: "JavaScript is complementary to and integrated with HTML",
+        choice2: "JavaScript is open and cross-platform",
+        choice3: "Both of the above",
+        choice4: "None of the above",
+        answer: "Both of the above"
+    },
+    {
+        question: "Which built-in method removes the last element from an array and returns that element?",
+        choice1: "last()",
+        choice2: "get()",
+        choice3: "pop()",
+        choice4: "None of the above",
+        answer: "pop()"
+    },
+    {
+        question: "Which of the following functions splits objects into an array of strings",
+        choice1: "slice()",
+        choice2: "split()",
+        choice3: "replace()",
+        choice4: "search()",
+        answer: "split()"
+    },
+    {
+        question: "Which functions returns the string value as upper case?",
+        choice1: "toLocaleUpperCase()",
+        choice2: "toUpperCase()",
+        choice3: "toString()",
+        choice4: "subString()",
+        answer: "toUpperCase()"
+    },
+    {
+        question: "____ tag is an extension of HTML that can enclose any number of JavaScript statements",
+        choice1: "<script>",
+        choice2: "<body>",
+        choice3: "<head>",
+        choice4: "<title>",
+        answer: "<script>"
+    },
+]
 
 var timerEl = document.querySelector("#time");
 var penalty = 10;
 
 var question = document.querySelector("#question");
 var choices = Array.from(document.querySelectorAll(".choice-text"));
-var scoreText= document.querySelector("#time");
+var scoreText = document.querySelector("#time");
 
 var currentQuestion = {}
 var acceptingAnswers = true;
@@ -59,15 +59,15 @@ var secondsLeft = 60;
 
 function startTimer() {
     // Sets interval in variable
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timerEl.textContent = secondsLeft;
-  
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-      }
-  
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timerEl.textContent = secondsLeft;
+
+        if (secondsLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
+        }
+
     }, 1000);
 }
 
@@ -79,48 +79,48 @@ function startGame() {
 }
 
 function getNewQuestion() {
-    if(availableQuestions.length === 0 || questionCounter > maxQuestions) {
+    if (availableQuestions.length === 0 || questionCounter > maxQuestions) {
         localStorage.setItem(secondsLeft, timerEl)
 
         return window.location.assign("high_score.html")
-}
+    }
 
-//randomizes questions
+    //randomizes questions
 
-var questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-currentQuestion = availableQuestions[questionsIndex]
-question.innerText = currentQuestion.question
+    var questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
 
-choices.forEach(choice => {
-    var number = choice.dataset["number"]
-    choice.innerText = currentQuestion["choice" + number]
+    choices.forEach(function (choice) {
+        var number = choice.dataset["number"]
+        choice.innerText = currentQuestion["choice" + number]
     })
 
     availableQuestions.splice(questionsIndex, 1);
 
-    acceptingAnswers = true; 
+    acceptingAnswers = true;
 }
 
 
-    choices.forEach(choice => {
-        choice.addEventListener("click", event => {
-            if(!acceptingAnswers) return;
+choices.forEach(function (choice) {
+    return choice.addEventListener("click", function (event) {
+        if (!acceptingAnswers) return;
 
-            acceptingAnswers = false;
-            var selectedChoice = event.target;
-            var selectedAnswer = selectedChoice.dataset["number"];
+        acceptingAnswers = false;
+        var selectedChoice = event.target;
+        var selectedAnswer = selectedChoice.dataset["number"];
 
-            var classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :
+        var classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :
             'incorrect';
 
-            selectedChoice.parentElement.classList.add(classToApply);
+        selectedChoice.parentElement.classList.add(classToApply);
 
-            setTimeout(() => {
-                selectedChoice.parentElement.classList.remove(classToApply);
-                getNewQuestion();
-            }, 100);
-        })
-    })
+        setTimeout(function () {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 100);
+    });
+})
 
 startTimer();
 
