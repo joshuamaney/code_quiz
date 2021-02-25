8d76093a8b604d26853406a7689a66a564fec78d
Joshua Maney <joshuaraymaney@gmail.com>
2021-02-25 01:12:56 -0500

    added notes to js for clarity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,30 @@
+var question = document.getElementById("question");
+// selecting choices by passing in array
+var choices = Array.from(document.getElementsByClassName("choice-text"));
+console.log(choices);
+// selects timer
+var timerEl = document.querySelector("#time");
+// current question object
+var currentQuestion = {};
+// adds delay after selecting answers, false so answers not permitted until everything is loaded
+var acceptingAnswers = false;
+var score = 0;
+// tracks number of question
+var questionCounter = 0;
+// empty array that will pull questions from questions set
+var availableQuestions = [];
+var secondsLeft = 60;
+var penalty = 10;
+// question and answer array
 var questions = [
+    // each question is an object
     {
         question: "Which of the following is correct about features of JavaScript?",
         choice1: "JavaScript is complementary to and integrated with HTML",
         choice2: "JavaScript is open and cross-platform",
         choice3: "Both of the above",
         choice4: "None of the above",
-        answer: "Both of the above"
+        answer: 3
     },
     {
         question: "Which built-in method removes the last element from an array and returns that element?",
@@ -13,7 +32,7 @@ var questions = [
         choice2: "get()",
         choice3: "pop()",
         choice4: "None of the above",
-        answer: "pop()"
+        answer: 3
     },
     {
         question: "Which of the following functions splits objects into an array of strings",
@@ -21,7 +40,7 @@ var questions = [
         choice2: "split()",
         choice3: "replace()",
         choice4: "search()",
-        answer: "split()"
+        answer: 2
     },
     {
         question: "Which functions returns the string value as upper case?",
@@ -29,7 +48,7 @@ var questions = [
         choice2: "toUpperCase()",
         choice3: "toString()",
         choice4: "subString()",
-        answer: "toUpperCase()"
+        answer: 2
     },
     {
         question: "____ tag is an extension of HTML that can enclose any number of JavaScript statements",
@@ -37,94 +56,72 @@ var questions = [
         choice2: "<body>",
         choice3: "<head>",
         choice4: "<title>",
-        answer: "<script>"
+        answer: 1
     },
 ]
 
-var timerEl = document.querySelector("#time");
-var question = document.querySelector("#question");
-var choices = Array.from(document.querySelectorAll(".choice-text"));
-var scoreText = document.querySelector("#time");
-
-var currentQuestion = {}
-var acceptingAnswers = true;
-var questionsArray = [];
-
-var secondsLeft = 60;
-var penalty = 10;
+function startQuiz() {
+    // reset
+    questionCounter = 0;
+    // copies all the questions from the questions array using a spread operator
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    getNewQuestion();
+}
 
 function startTimer() {
     // Sets interval in variable
     var timerInterval = setInterval(function () {
         secondsLeft--;
         timerEl.textContent = secondsLeft;
-
+    
         if (secondsLeft === 0) {
-            // Stops execution of action at set interval
+        // Stops execution of action at set interval
             clearInterval(timerInterval);
         }
-
+    
     }, 1000);
 }
 
-function startGame() {
-    time = 0
-    questionsArray = [...questions];
-    getNewQuestion();
-}
-
 function getNewQuestion() {
-    if (questionsArray.length === 0) {
-        localStorage.setItem(secondsLeft, timerEl)
-
-        return window.location.assign("high_score.html")
+    // check if out of questions, then go to highscore page
+    if(availableQuestions.length === 0) { 
+        return window.location.assign("./high_score.html");
     }
+    // increments the question
+    questionCounter++;
+    // get random question
+    var questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    // populates question in question id
+    question.innerText = currentQuestion.question;
 
-    //randomizes questions
-
-    var questionsIndex = Math.floor(Math.random() * questionsArray.length)
-    currentQuestion = questionsArray[questionsIndex]
-    question.innerText = currentQuestion.question
-
-    choices.forEach(function (userChoice) {
-        var number = userChoice.dataset["number"]
-        userChoice.innerText = currentQuestion["choice" + number]
-    })
-
-    questionsArray.splice(questionsIndex, 1);
-
+    choices.forEach(function(userChoice) {
+    // accesses appropriate choice dataset attribute by number and populates choice-text class
+    var number = userChoice.dataset["number"];
+    userChoice.innerText = currentQuestion["choice" + number];
+    });
+    // "splices out" the question that was just displayed
+    availableQuestions.splice(questionIndex, 1);
+    // setting acceptingAnswers to true to allow user to answer
     acceptingAnswers = true;
 }
 
-
-choices.forEach(function (userChoice) {
-        userChoice.addEventListener("click", function (event) {
-        if (!acceptingAnswers) return;
+// grabs each choice, adds event listener to listen for each choice that is clicked
+choices.forEach(function(userChoice) {
+    userChoice.addEventListener("click", function(event) {
+        console.log(event.target);
+        // user can't click until loading is finished
+        if(!acceptingAnswers) return;
 
         acceptingAnswers = false;
+        // what the user chooses
         var selectedChoice = event.target;
         var selectedAnswer = selectedChoice.dataset["number"];
+        console.log("selectedAnswer");
+        getNewQuestion();
+    });
+});
 
-        var classToApply = selectedAnswer 
-
-        if (selectedChoice === "incorrect") {
-            penalty();
-        }
-
-        selectedChoice.parentElement.classList.add(classToApply);
-
-        setTimeout(function () {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 100)
-    })
-})
-
-function penalty() {
-    timerEl = time - penalty;
-}
-
-
+startQuiz();
 startTimer();
-
-startGame();
